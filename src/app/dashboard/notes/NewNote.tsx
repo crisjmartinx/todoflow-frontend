5b228e40d75031ed9f79001ec8758b2146f2bae2
Cristian <theiron_maiden_6@hotmail.com>
2025-03-13 00:50:57 -0300

    fixes UI

--- src/app/dashboard/notes/NewNote.tsx
+++ src/app/dashboard/notes/NewNote.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useRef, useState } from "react";
 
 import { TagCategory, TagItem } from "@/types";
 
-import { ChevronLeft, Tags } from "lucide-react";
+import { ChevronLeft, Sparkles, Tags } from "lucide-react";
 import { Editor } from "@/components/Editor";
 import { createNote, updateNote } from "@/actions/note-actions";
 import { useNoteAutoSave } from "@/hooks/useAutoSave";
@@ -125,6 +125,10 @@ export const NewNote: React.FC<NewNoteProps> = ({
     }));
   };
 
+  const handleInteligence = () => {
+    // setActivateIA((prev) => !prev);
+  };
+
   useNoteAutoSave({
     formData,
     createData: createNote,
@@ -136,15 +140,10 @@ export const NewNote: React.FC<NewNoteProps> = ({
   return (
     <div
       ref={modalRef}
-      className="container-modal bg-[#00000000] translate-y-[1200px] backdrop-blur-[100px] h-[-webkit-fill-available] w-[-webkit-fill-available] fixed z-[100] overflow-hidden flex justify-center items-center p-[0.3rem] top-[12.2rem] md:top-[9.6rem]"
-      style={{
-        background: activateIA
-          ? `linear-gradient(125deg, rgba(255, 0, 92, 0.7), rgba(255, 121, 63, 0.7), rgba(255, 210, 0, 0.7), rgba(46, 196, 182, 0.7), rgba(36, 123, 160, 0.7), rgba(96, 17, 145, 0.7), rgba(255, 0, 153, 0.7), rgba(255, 77, 0, 0.7), rgba(255, 0, 92, 0.7), rgba(255, 178, 82, 0.7), rgba(0, 204, 255, 0.7), rgba(255, 165, 0, 0.7), rgba(170, 0, 255, 0.7), rgba(255, 51, 204, 0.7))`
-          : "transparent",
-        backgroundSize: "400% 400%",
-        animation: "rotateBackground 8s linear infinite",
-      }}
+      className={`container-modal bg-[#00000000] translate-y-[1200px] backdrop-blur-[100px] h-[-webkit-fill-available] w-[-webkit-fill-available] fixed z-[100] overflow-hidden flex justify-center items-center p-[0.3rem] top-[12.2rem] md:top-[9.6rem]`}
     >
+      {activateIA && <div className={"ia-generate"}></div>}
+
       {loadingSave && <div className="loader"></div>}
       <form className="flex flex-col bg-[#FFF] p-6 rounded-[5px] w-full h-full">
         <input
@@ -258,13 +257,32 @@ export const NewNote: React.FC<NewNoteProps> = ({
           )}
         </div>
 
-        <div className="pt-5">
-          <span className="text-black font-semibold text-sm select-none">
-            Nota
-          </span>
-        </div>
+        <div className="pt-5 mb-2">
+          <div className="flex justify-between">
+            <span className="text-black font-semibold text-sm select-none">
+              Nota
+            </span>
 
-        <div className="flex-1 pt-2 relative group">
+            <button
+              type="button"
+              className="flex items-center justify-center gap-2 text-white bg-black font-extralight text-[0.8rem] px-3 py-2 rounded-lg transition"
+              onClick={handleInteligence}
+            >
+              <Sparkles color="#fff" size={15} />
+
+              {activateIA ? (
+                <span>Generando...</span>
+              ) : (
+                <span>Intelligence</span>
+              )}
+            </button>
+          </div>
+        </div>
+        <div
+          className={`flex-1 pt-2 relative group ${
+            activateIA ? "intelligence-container" : ""
+          }`}
+        >
           <Editor handleInput={handleInput} content={formData.content} />
         </div>
       </form>
