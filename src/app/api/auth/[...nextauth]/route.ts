cda979d0a5a3829e2c37aa1d1772faee1410782a
Cristian <theiron_maiden_6@hotmail.com>
2025-03-12 22:13:04 -0300

    fix auth router

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,53 +1,62 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+// import NextAuth, { NextAuthOptions } from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "email", type: "email", placeholder: "email" },
-        password: { label: "password", type: "password" },
-      },
-      async authorize(credentials) {
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`,
-          {
-            method: "POST",
-            body: JSON.stringify({
-              email: credentials?.email,
-              password: credentials?.password,
-            }),
-            headers: { "Content-Type": "application/json" },
-            credentials: "include",
-          }
-        );
-        const data = await res.json();
+// export const authOptions: NextAuthOptions = {
+//   providers: [
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         email: { label: "email", type: "email", placeholder: "email" },
+//         password: { label: "password", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         const res = await fetch(
+//           `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`,
+//           {
+//             method: "POST",
+//             body: JSON.stringify({
+//               email: credentials?.email,
+//               password: credentials?.password,
+//             }),
+//             headers: { "Content-Type": "application/json" },
+//             credentials: "include",
+//           }
+//         );
+//         const data = await res.json();
 
-        if (data.error) throw data;
+//         if (data.error) throw data;
 
-        return {
-          id: data.user.id,
-          email: data.user.email,
-          name: data.user.name,
-          token: data.token,
-        };
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      return { ...token, ...user };
-    },
-    async session({ session, token }) {
-      session.user = token as any;
-      return session;
-    },
-  },
-  pages: {
-    signIn: "/",
-  },
-};
+//         return {
+//           id: data.user.id,
+//           email: data.user.email,
+//           name: data.user.name,
+//           token: data.token,
+//         };
+//       },
+//     }),
+//   ],
+//   callbacks: {
+//     async jwt({ token, user }) {
+//       return { ...token, ...user };
+//     },
+//     async session({ session, token }) {
+//       session.user = token as any;
+//       return session;
+//     },
+//   },
+//   pages: {
+//     signIn: "/",
+//   },
+// };
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
+
+
+import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth";
+ // Asegúrate de que la ruta es correcta
 
 const handler = NextAuth(authOptions);
 
