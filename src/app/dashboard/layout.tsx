a7877eb2481d0d5f3ebcc47443bdf11e1532918c
Cristian <theiron_maiden_6@hotmail.com>
2025-06-04 18:42:32 -0300

    eliminar espacios en blanco innecesarios en el componente DashboardLayout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,14 +3,11 @@ import React from "react";
 import Header from "@/components/main/header/Header";
 import Sidebar from "@/components/main/sidebar/Sidebar";
 
-
-
 interface DashboardLayout {
   children: React.ReactNode;
 }
 
 export default async function DashboardLayout({ children }: DashboardLayout) {
-  
   return (
     <div>
       <Header />
